a60028a26360bc6fc67ee2dbc5b2c1d34cb3931e
Roman <sayga76239@gmail.com>
2023-02-12 18:19:15 +0100

    startList

---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  max-width: 360px;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: aqua;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+`;
+
+export const Input = styled.input`
+  width: 200px;
+  margin-top: 2px;
+`;
+
+export const Button = styled.button`
+  cursor: pointer;
+  `;
